d3d4f656520987dfbc8ae08e905616452484745d
tonney195 <newtonkips8@gmail.com>
2025-09-20 14:19:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Newton",
   botOwner: 'Tonney', // Your name
-  ownerNumber: '254759449085', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2547594490', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
