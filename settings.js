294ce45ca809377fa78e09af0ed820d681c49bd6
tonney195 <newtonkips8@gmail.com>
2025-09-26 11:02:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Tonnie Bot',
+  packname: 'Newton',
   author: '‎',
   botName: "Keisy Bot",
   botOwner: 'Tonney', // Your name
   ownerNumber: '254759449085', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  maxStoreMessages: 70, 
+  maxStoreMessages: 25, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.2.0",
